c7763fc49ccd1af6800935e7270b0a8c8d88d42f
Purushothamr22 <Purushothamr22.xworkz@gmail.com>
2024-02-28 13:25:03 +0530

    1

---
+++ VeriluxDevice.java
@@ -0,0 +1,47 @@
+class VeriluxDevice {
+
+    public static void main(String usb[]) {
+
+        String brand = "Verilux";
+        String manufacturer = "Verilux, cs.service01outlook.com";
+        String model = "AUB09YTB1DSB";
+        String productDimensions = "2.1 x 0.65 x 0.15 cm";
+        String itemModelNumber = "AUB09YTB1DSB";
+        String operatingSystem = "Linux, Windows 8, Mac OS, Windows 10";
+        String hardwarePlatform = "Laptop";
+        String hardwareInterface = "USB";
+        String compatibleDevices = "Laptop, Personal Computer";
+        String specialFeatures = "Wireless";
+        String powerSource = "Corded Electric";
+        String batteryAverageLife = "6 months";
+        boolean isBatteriesIncluded = false;
+        boolean isBatteriesRequired = false;
+        String connectorType = "USB";
+        String movementDetectionTechnology = "Optical";
+        String material = "Aluminium, Plastic, Metal";
+        String handOrientation = "Ambidextrous";
+        String countryOfOrigin = "China";
+        String itemWeight = "3 grams";
+
+        System.out.println("Brand: " + brand);
+        System.out.println("Manufacturer: " + manufacturer);
+        System.out.println("Model: " + model);
+        System.out.println("Product Dimensions: " + productDimensions);
+        System.out.println("Item Model Number: " + itemModelNumber);
+        System.out.println("Operating System: " + operatingSystem);
+        System.out.println("Hardware Platform: " + hardwarePlatform);
+        System.out.println("Hardware Interface: " + hardwareInterface);
+        System.out.println("Compatible Devices: " + compatibleDevices);
+        System.out.println("Special Features: " + specialFeatures);
+        System.out.println("Power Source: " + powerSource);
+        System.out.println("Battery Average Life: " + batteryAverageLife);
+        System.out.println("Batteries Included: " + isBatteriesIncluded);
+        System.out.println("Batteries Required: " + isBatteriesIncludeds);
+        System.out.println("Connector Type: " + connectorType);
+        System.out.println("Movement Detection Technology: " + movementDetectionTechnology);
+        System.out.println("Material: " + material);
+        System.out.println("Hand Orientation: " + handOrientation);
+        System.out.println("Country of Origin: " + countryOfOrigin);
+        System.out.println("Item Weight: " + itemWeight );
+    }
+}
