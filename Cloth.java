c24d15f727beb62ffe06049d7f280d6717dfe8fe
Purushothamr22 <Purushothamr22.xworkz@gmail.com>
2024-03-18 10:37:09 +0530

    objects

---
+++ Cloth.java
@@ -0,0 +1,28 @@
+class Cloth{
+
+  String brandName ;
+  String material ;
+  double price;
+  String size ;
+  String color ;
+  String type ;
+  
+  public Cloth(){
+   System.out.println(" cloth unique design ");
+   
+  } 
+   
+  public void display(){
+    
+	System.out.println(" Brand Name is : "+ brandName);
+	System.out.println(" Material is : "+ material);
+	System.out.println(" Price  is : "+ price);
+	System.out.println(" Size is : "+ size);
+	System.out.println(" Color of cloth  is : "+ color);
+	System.out.println(" Type of cloth  is : "+ type);
+    
+  }
+ 
+
+
+}
---
+++ ClothTester.java
@@ -0,0 +1,57 @@
+class ClothTester{
+
+  public static void main(String clothCollection[]){
+	  
+	  System.out.println(" main method started ");
+	  
+	 /* Cloth cloth = new Cloth();
+	  
+	   cloth.display();
+	   cloth.brandName = "Levi's";
+	   cloth.material = "Cotton";
+	   cloth.price = 2000.00;
+	   cloth.size = "M";
+	   cloth.color = "Black";
+	   cloth.type = "casual";
+	   cloth.display();
+	   
+	   
+	   Cloth cloth1 = new Cloth();
+	  
+	   cloth1.display();
+	   cloth1.brandName = "Puma";
+	   cloth1.material = "Cotton";
+	   cloth1.price = 2500.00;
+	   cloth1.size = "L";
+	   cloth1.color = "Blue";
+	   cloth1.type = "casual";
+	   cloth1.display();
+	   
+	   
+	   Cloth cloth2 = new Cloth();
+	  
+	   cloth2.display();
+	   cloth2.brandName = "Nike";
+	   cloth2.material = "Nylon";
+	   cloth2.price = 2200.00;
+	   cloth2.size = "M";
+	   cloth2.color = "Black";
+	   cloth2.type = "Sport ware";
+	   cloth2.display();*/
+	   
+	   
+	   
+	   
+	   
+	   
+	   
+	   
+	   
+	  System.out.println(" end of main ");
+	  
+	  
+	  
+	  
+  }
+
+}
